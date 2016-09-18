9bde88d0c0e98da40ab89a5b9ebbe05775fa3e71
Miranda Hawkins <mirhawk@Mirandas-MacBook-Pro.local>
2016-09-17 22:50:06 -0400

    Solidified categories

--- scrape/soup.py
+++ scrape/soup.py
@@ -5,31 +5,32 @@ import os
 
 URL = "https://www.google.com/search?hl=en&site=imghp&tbm=isch&source=hp&q="
 URL_PAGE = "&start="
-queries = ["baroque+painting", "rococo+painting", "impressionism", "surrealism", "memes"]
-curr_page = 100
+#queries = ["baroque+painting", "rococo+painting", "impressionism", "surrealism", "memes"]
+queries = ["medieval+painting","japanese+woodblock"]
+curr_page = 0
 
 for query in queries: 
 	imgs = set()
 
-	while curr_page < 300:
+	while curr_page < 400:
 
 		req = Request(URL + query + URL_PAGE + str(curr_page) ,headers={ 'User-Agent' : 'Mozilla/5.0' })
 		html = urlopen(req)
 
 		soup = BeautifulSoup(html)
 
-		for i in soup.find_all("img", limit=200):
+		for i in soup.find_all("img", limit=400):
 			imgs.add(i.get('src'))
 
 		curr_page = len(imgs)
 
-	#os.mkdir("data/"+query)
+	os.mkdir("data/"+query)
 
 	for i in range(len(imgs)):
 		resource = urlopen(imgs.pop())
-		output = open("data/"+query+"/"+query+"-img-"+str(i+100)+".jpg", "wb")
+		output = open("data/"+query+"/"+query+"-img-"+str(i)+".jpg", "wb")
 		output.write(resource.read())
 		output.close()
 
-	curr_page = 100;
+	curr_page = 0;
 
